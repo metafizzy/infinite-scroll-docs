1f149e341383fdbeed8edace86189fbf9296d7c9
David DeSandro <desandrocodes@gmail.com>
2017-05-25 10:41:00 -0400

    🎩 full page demo

--- tasks/utils/add-file-properties.js
+++ tasks/utils/add-file-properties.js
@@ -5,8 +5,12 @@ var path = require('path');
 module.exports = function() {
   return transfob( function( file, enc, next ) {
     file.basename = path.basename( file.path, '.hbs' );
-    file.rootPath = path.relative( file.path, file.cwd + '/content/' )
-      .replace( /\.\.$/, '' );
+    if ( file.data.page.is404 ) {
+      file.rootPath = '/';
+    } else {
+      file.rootPath = path.relative( file.path, file.cwd + '/content/' )
+        .replace( /\.\.$/, '' );
+    }
     next( null, file );
   });
 };
