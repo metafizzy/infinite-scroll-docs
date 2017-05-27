ac736a78329315f905b741a8314291c6f20c568f
David DeSandro <desandrocodes@gmail.com>
2017-05-26 20:40:08 -0400

    🎩 improve masonry images demo

--- modules/image-grid/image-grid.js
+++ modules/image-grid/image-grid.js
@@ -2,82 +2,32 @@
 
 EpiscrollDocs['image-grid'] = function( elem ) {
 
-  // --------------------------  -------------------------- //
-
-  var proto = Episcroll.prototype;
-
-  proto.onAppendHistory = function( response, path, items ) {
-    var index = this.scrollPages.length;
-    this.updateHistoryPage( response, path, items, index );
-  };
-
-  proto.updateHistoryPage = function( response, path, items, pageIndex ) {
-    pageIndex = pageIndex === undefined ? this.loadCount : pageIndex;
-    var firstItem = items[0];
-    var elemScrollY = this.getElementScrollY( firstItem );
-    // resolve path
-    link.href = path;
-    // add page data to hash
-    this.scrollPages[ pageIndex ] = {
-      top: elemScrollY,
-      path: link.href,
-      title: response.title,
-    };
-  };
-  
-  // --------------------------  -------------------------- //
-
   var msnry = new Masonry( elem, {
-    itemSelector: '.image-grid__item',
+    itemSelector: 'none', // select none at first
     columnWidth: '.image-grid__col-sizer',
     gutter: '.image-grid__gutter-sizer',
     percentPosition: true,
     stagger: 30,
+    // nicer reveal transition
+    visibleStyle: { transform: 'translateY(0)', opacity: 1 },
+    hiddenStyle: { transform: 'translateY(100px)', opacity: 0 },
   });
 
   imagesLoaded( elem, function() {
-    msnry.layout();
+    // select items now
+    elem.classList.remove('are-images-unloaded');
+    msnry.options.itemSelector = '.image-grid__item';
+    var items = elem.querySelectorAll('.image-grid__item');
+    msnry.appended( items );
   });
 
   var infScroll = new Episcroll( elem, {
     path: '.pagination__next',
     hideNav: '.pagination',
     append: '.image-grid__item',
+    outlayer: msnry,
+    status: '.scroller__status',
     log: true,
-    // scrollThresold: 1,
-    history: false,
   });
-
-
-  infScroll.on( 'append', function( response, path, items ) {
-    var loadCount = infScroll.loadCount;
-    infScroll.options.loadOnScroll = false;
-    setItemsDisplay( items, 'none' );
-    imagesLoaded( items, function() {
-      setTimeout( function() {
-        setItemsDisplay( items, 'block' );
-        msnry.appended( items );
-        infScroll.updateHistoryPage( response, path, items, loadCount );
-        infScroll.options.loadOnScroll = true;
-      }, 1000 );
-    })
-  });
-
-  function setItemsDisplay( items, value ) {
-    for ( var i=0; i < items.length; i++ ) {
-      items[i].style.display = value;
-    }
-  }
-
-  // infScroll.on( 'load', function( response ) {
-  //   var items = response.querySelectorAll('.image-grid__item');
-  //   // append items to fragment for imagesLoaded
-  //   // TODO check if necessary with jQuery
-  //   infScroll.getItemsFragment( items );
-  //   imagesLoaded( items, function() {
-  //     infScroll.appendItems( items );
-  //     msnry.appended( items );
-  //   });
-  // });
 
 };
