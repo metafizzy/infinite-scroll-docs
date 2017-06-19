7d2d7f94b6efd71fb611a060c93b78f5b37486ab
David DeSandro <desandrocodes@gmail.com>
2017-06-18 21:27:05 -0400

    🍹 add back dist task

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,16 @@ var gulp = require('gulp');
 
 // ----- site ----- //
 
+var taskName = process.argv[2];
+
 // stuff used across tasks
 var site = {
   // templating data
   data: {
     productName: 'Infinite Scroll',
     majorVersion: 3,
-    isDev: process.argv[2] == 'dev',
-    isExport: process.argv[2] == 'export',
+    isDev: taskName == 'dev',
+    isExport: taskName == 'export',
   },
   // src to watch, tasks to trigger
   watches: [],
@@ -21,7 +23,7 @@ var site = {
 // ----- tasks ----- //
 
 require('./tasks/assets')( site );
-// require('./tasks/dist')( site );
+require('./tasks/dist')( site );
 require('./tasks/hint')( site );
 require('./tasks/js')( site );
 require('./tasks/css')( site );
@@ -34,21 +36,20 @@ gulp.task( 'default', [
   'content',
   'js',
   'css',
-  // 'dist',
+  'dist',
   'prod-assets'
 ] );
 
 // ----- export ----- //
 
 // version of site used in infinite-scroll-docs.zip
-
 gulp.task( 'export', [ 'default' ] );
 
 // ----- watch ----- //
 
 gulp.task( 'dev', [
   'hint',
-  // 'dist',
+  'dist',
   'prod-assets',
   'content'
 ], function() {
